be179af132482dd9feecb6500d196bfb93a2236b
Colin Casey <casey.colin@gmail.com>
2023-07-26 15:54:29 -0300

    Fix rust caching and duplicate buildpack compilation
    This is a restructuring of the whole release process to eliminate the issues outlines in #48 (buildpacks compiled multiple times could result in non-determinism) and #49 (rust caching isn't working due to non-unique keys).
    
    Many of the overall steps are still the same but the `detect` job has been replaced by a `compile` job which front loads all the rust and buildpack compilation and cached the compiled buildpack folder for later use. Compilation was previously handled in the `package` job which is no longer needed.
    
    To support this new flow, a `buildpack_output_dir` value had to be added to the generated buildpack matrix. This also allowed some simplification in terms of the assets cached since the OCI image and CNB file can be generated directly from the compiled `buildpack_output_dir` we don't need to store those values in the cache anymore.

--- src/commands/generate_buildpack_matrix/command.rs
+++ src/commands/generate_buildpack_matrix/command.rs
@@ -2,7 +2,9 @@ use crate::buildpacks::{find_releasable_buildpacks, read_image_repository_metada
 use crate::commands::generate_buildpack_matrix::errors::Error;
 use crate::github::actions;
 use clap::Parser;
-use libcnb_package::{read_buildpack_data, BuildpackData, GenericMetadata};
+use libcnb_package::{
+    get_buildpack_target_dir, read_buildpack_data, BuildpackData, GenericMetadata,
+};
 use std::collections::{BTreeMap, HashSet};
 use std::path::Path;
 
@@ -26,7 +28,7 @@ pub(crate) fn execute(_: GenerateBuildpackMatrixArgs) -> Result<()> {
     let includes = buildpack_dirs
         .iter()
         .zip(buildpacks.iter())
-        .map(|(dir, data)| extract_buildpack_info(data, dir))
+        .map(|(dir, data)| extract_buildpack_info(data, dir, &current_dir))
         .collect::<Result<Vec<_>>>()?;
 
     let includes_json = serde_json::to_string(&includes).map_err(Error::SerializingJson)?;
@@ -55,16 +57,13 @@ pub(crate) fn execute(_: GenerateBuildpackMatrixArgs) -> Result<()> {
 pub(crate) fn extract_buildpack_info(
     buildpack_data: &BuildpackData<GenericMetadata>,
     dir: &Path,
+    workspace_dir: &Path,
 ) -> Result<BTreeMap<String, String>> {
     let buildpack_dir = dir.to_string_lossy().to_string();
 
     let buildpack_path = buildpack_data.buildpack_descriptor_path.clone();
 
-    let buildpack_id = buildpack_data
-        .buildpack_descriptor
-        .buildpack()
-        .id
-        .to_string();
+    let buildpack_id = buildpack_data.buildpack_descriptor.buildpack().id.clone();
 
     let buildpack_version = buildpack_data
         .buildpack_descriptor
@@ -77,13 +76,23 @@ pub(crate) fn extract_buildpack_info(
     let docker_repository = read_image_repository_metadata(&buildpack_data.buildpack_descriptor)
         .ok_or(Error::MissingDockerRepositoryMetadata(buildpack_path))?;
 
+    let buildpack_output_dir = get_buildpack_target_dir(
+        &buildpack_id,
+        &workspace_dir.to_path_buf().join("target"),
+        true,
+    );
+
     Ok(BTreeMap::from([
-        ("buildpack_id".to_string(), buildpack_id),
+        ("buildpack_id".to_string(), buildpack_id.to_string()),
         ("buildpack_version".to_string(), buildpack_version),
         ("buildpack_dir".to_string(), buildpack_dir),
         (
             "buildpack_artifact_prefix".to_string(),
             buildpack_artifact_prefix,
+        ),
+        (
+            "buildpack_output_dir".to_string(),
+            buildpack_output_dir.to_string_lossy().to_string(),
         ),
         ("docker_repository".to_string(), docker_repository),
     ]))
